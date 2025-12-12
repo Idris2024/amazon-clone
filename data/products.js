8dbe06161a094a7906572eb2b5fff8e8354ff3f8
Idris2024 <navasarowolo@gmail.com>
2025-12-12 18:17:04 +0100

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -86,6 +86,31 @@ const object3 = {
 object3.method();
 */
 
+// BACKEND
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+  products = JSON.parse(xhr.response).map((productDetails) => {
+    if (productDetails.type === 'clothing') {
+      return new clothing(productDetails);
+    }
+    return new Product(productDetails);
+  });
+
+  console.log('load products');
+
+  fun();
+});
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -751,4 +776,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
- 
+*/
